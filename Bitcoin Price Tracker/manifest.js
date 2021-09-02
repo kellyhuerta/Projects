14ae23f8e02e0ffd7a13f1c7d1d55016bc21ed03
Kelly Huerta <80060515+kellyhuerta@users.noreply.github.com>
2021-09-02 15:42:51 -0400

    Create manifest.js

---
+++ Bitcoin Price Tracker/manifest.js
@@ -0,0 +1,12 @@
+{
+  "name": "Bitcoin Price Tracker",
+  "manifest_version": 2,
+  "version": "1.0",
+  "description": "Chrome Extension which tracks Bitcoin prices",
+  "browser_action": {
+		"default_name": "Bitcoin Price Tracker",
+    "default_icon": "bitcoin.png",
+    "default_popup": "index.html"
+  },
+  "permissions": []
+}
